9d9d3ad4e06c5e448a57b85c1f905c2df034eb8f
gautamk1512 <77240480+gautamk1512@users.noreply.github.com>
2021-04-26 20:19:56 +0530

    Inital commit

--- textutils/settings.py
+++ textutils/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'm40miqw+j^8m&bquf=%hgpv6d**s6cyx4!$xj^y%0oeg5_p)eo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -121,6 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-django_heroku.settings(locals))
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# # django_heroku.settings(locals)
